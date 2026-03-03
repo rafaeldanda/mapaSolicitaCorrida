1c36176617e34380c6da45dc5aed0fc5e42b9c3d
rafaeldanda <rafaeldanda@gmail.com>
2026-03-03 13:07:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => new Response("Offline")));
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Sem esse evento de fetch, o botão não aparece
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => new Response("Offline"))
+    );
 });
