223308e3c7ae007e655dd2e229d7610b10f5d5eb
rafaeldanda <rafaeldanda@gmail.com>
2026-03-03 11:38:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-const CACHE_NAME = 'me-leva-v1';
+const CACHE_NAME = 'meleva-v1';
 
-// Instala o Service Worker e limpa caches antigos
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
-// Essencial para o Chrome liberar o botão de "Instalar"
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Essencial para o Chrome habilitar o botão de instalação
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request).catch(() => {
-            return new Response("Você está offline, mas o app continua tentando conectar.");
+            return new Response("Offline");
         })
     );
 });
