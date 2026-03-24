2741e22f81132d5bac8b8990340dd59240463355
rafaeldanda <rafaeldanda@gmail.com>
2026-03-24 13:21:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,28 @@
-const CACHE_NAME = "meleva-cache-v1";
+const CACHE_NAME = "meleva-v2"; // muda versão sempre
 
 self.addEventListener("install", event => {
+self.skipWaiting(); // ativa novo SW imediatamente
+});
+
+self.addEventListener("activate", event => {
 event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll([
-"./",
-"./index.html",
-"./manifest.json"
-]);
+caches.keys().then(keys => {
+return Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
 })
 );
+})
+);
+
+```
+self.clients.claim(); // assume controle imediato
+```
+
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-fetch(event.request).catch(() => caches.match(event.request))
-);
+event.respondWith(fetch(event.request));
 });
