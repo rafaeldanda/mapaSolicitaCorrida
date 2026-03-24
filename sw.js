18c79231ec468888dd295a4eab6ba4b4148fe1a8
rafaeldanda <rafaeldanda@gmail.com>
2026-03-24 13:19:29 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'me-leva-v3';
+const CACHE_NAME = "meleva-cache-v1";
 
-self.addEventListener('install', (e) => self.skipWaiting());
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => {
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.json"
+]);
+})
+);
 });
 
-// Essa função permite que o Service Worker receba mensagens do Front-end
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'START_TRACKING') {
-        // Cria uma notificação persistente (Isso ajuda o Android a não matar o processo)
-        console.log("Rastreamento em segundo plano iniciado no SW");
-    }
-});
-
-self.addEventListener('fetch', (e) => {
-    // Mantém as requisições de rede passando sem travas
-    e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+event.respondWith(
+fetch(event.request).catch(() => caches.match(event.request))
+);
 });
